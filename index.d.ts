8ddc0a41d1b74226e2572343b3b14b29f794cfab
Awal Ariansyah <awalariansyah7@gmail.com>
2024-08-02 09:52:25 +0700

    add event emitter

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,6 @@ declare const STABLE_WEB_VERSION: string;
 declare const STABLE_WWJS_VERSION: string;
 
 declare const SClient: Client;
-export { SClient, STABLE_WEB_VERSION, STABLE_WWJS_VERSION };
+declare const onReady: () => void;
+
+export { onReady, SClient, STABLE_WEB_VERSION, STABLE_WWJS_VERSION };
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from "events";
 import qrcode from "qrcode-terminal";
 import { Client, LocalAuth } from "whatsapp-web.js";
 
@@ -21,10 +22,17 @@ SClient.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
+const eventEmitter = new EventEmitter();
+
+function onReady(callback: () => void) {
+  eventEmitter.on("ready", callback);
+}
+
 SClient.on("ready", () => {
   console.log("[LOG] WHATSAPP BOT IS RUNNING");
   console.log(`[LOG] WWJS VERSION: ${STABLE_WWJS_VERSION}`);
   console.log(`[LOG] WEB CACHE VERSION: ${STABLE_WEB_VERSION}`);
+  eventEmitter.emit("ready");
 });
 
-export { SClient, STABLE_WEB_VERSION, STABLE_WWJS_VERSION };
+export { onReady, SClient, STABLE_WEB_VERSION, STABLE_WWJS_VERSION };
